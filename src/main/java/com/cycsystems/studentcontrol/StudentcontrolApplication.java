8de06422366a3e00f82de1f6dba3cb06287f1cdb
Luis Rodas <emmanuelcamposeco@gmail.com>
2017-09-27 14:38:24 -0600

    Primer Commit

---
+++ src/main/java/com/cycsystems/studentcontrol/StudentcontrolApplication.java
@@ -0,0 +1,12 @@
+package com.cycsystems.studentcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentcontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentcontrolApplication.class, args);
+	}
+}
---
+++ src/main/java/com/cycsystems/studentcontrol/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.cycsystems.studentcontrol.repository;
+
+import java.io.Serializable;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cycsystems.studentcontrol.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Serializable>{
+
+}
